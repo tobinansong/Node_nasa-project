ba8c9a138ce5dfbfc3f072f63da12dead1c83d02
Tobin Ansong <tobin.ansong@gmail.com>
2023-07-26 10:40:20 -0400

    updated project files, before we added github actions

---
+++ server/src/routes/planets/planets.router.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const { 
+    httpGetAllPlanets, 
+}= require("../../controllers/planets/planets.controller");
+
+const planetsRouter = express.Router();
+
+planetsRouter.get("/", httpGetAllPlanets);
+
+module.exports = planetsRouter;
